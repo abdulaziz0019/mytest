0bbf5bf505f07b9b6c3c62f9297cef00082e41a6
abdulaziz <abdulazizalmohsen92@gmail.com>
2019-08-17 19:08:07 +0300

    association with article and user

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+has_many :articles
+before_save{self.email =  email.downcase }
+  validates :username, presence:true , length:{minimum:1,maximum:10},uniqueness:{case_sensitive:false}
 
-end 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email , presence:true, uniqueness:{case_sensitive:false}, format:{ with: VALID_EMAIL_REGEX}
+
+end
