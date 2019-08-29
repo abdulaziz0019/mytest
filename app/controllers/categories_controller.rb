ddc13d7176c2734c7067b7715f95a1736f626882
abdulaziz <info@jaeeapp.com>
2019-08-29 21:24:16 +0300

    added category and edit it

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 
 
 before_action :set_category,only:[:show,:edit,:destroy,:update]
-before_action :require_admin, only:[:new,:create,:destroy,:update]
+before_action :require_admin, only:[:new,:create,:destroy,:update,:edit]
 
 def index
   @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -19,8 +19,20 @@ def create
 end
 
 def show
+  @category = Category.find(params[:id])
+  @article_categories = @category.articles.paginate(page: params[:page], per_page:4)
 end
 
+def edit
+
+end
+def update
+  if @category.update(all_params)
+    redirect_to category_path(@category)
+  else
+    render 'edit'
+  end
+end
 
 def destroy
 end
@@ -33,6 +45,8 @@ end
 def set_category
   @category = Category.find(params[:id])
 end
+
+
 private
 def require_admin
   if !logged_in || (logged_in and !current_user.admin?)
